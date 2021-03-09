7e033a64a1f48a21cbe5c8c26f2bbc3a4aa8f245
joraeng <60039286+joraeng@users.noreply.github.com>
2021-03-10 05:17:14 +0900

    템플릿 파일 생성

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
 //컨트롤러는 어떤 일이 어떻게 발생하는지에 관한 로직
 
 //global Router의 user 관련 기능 contorller
-export const join = (req, res) => res.send('Join') 
-export const login = (req, res) => res.send('Login')
-export const logout = (req, res) => res.send('Logout')
+export const join = (req, res) => res.render('join') 
+export const login = (req, res) => res.render('login')
+export const logout = (req, res) => res.render('logout')
 
 //user Router
-export const users = (req,res) => res.send('Users');
-export const userDetail = (req,res) => res.send("User Detail");
-export const editProfile = (req,res) => res.send("Edit Profile");
-export const changePassword = (req,res) => res.send("Change Password");
+export const users = (req,res) => res.render('users');
+export const userDetail = (req,res) => res.render("userDetail");
+export const editProfile = (req,res) => res.render("editProfile");
+export const changePassword = (req,res) => res.render("changePassword");
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,11 @@
 //home의 video 관련 기능들
-export const home = (req,res) => res.send("Home"); // home 컨트롤러
-export const search = (req,res) => res.send("Search"); 
-
+export const home = (req,res) => res.render("home"); // home 컨트롤러
+export const search = (req,res) => res.render("search"); 
 
 
 //video Rotuer 
-export const videos = (req,res) => res.send("Videos");
-export const upload = (req,res) => res.send("Upload");
-export const videoDetail = (req,res) => res.send("Video Detail");
-export const editVideo = (req,res) => res.send("Edit Video");
-export const deleteVideo = (req,res) => res.send("Delete Video");
+export const videos = (req,res) => res.render("videos");
+export const upload = (req,res) => res.render("upload");
+export const videoDetail = (req,res) => res.render("videoDetail");
+export const editVideo = (req,res) => res.render("editVideo");
+export const deleteVideo = (req,res) => res.render("deleteVideo");
