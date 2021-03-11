86ce4f3833c7fc675801cc5c692b81af0672e34d
joraeng <60039286+joraeng@users.noreply.github.com>
2021-03-12 01:49:25 +0900

    locals 활용해서 routes랑 템플릿 연결

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from './middlewares';
 import userRouter from "./routers/userRouter"; // default로 export하지 않으면 이렇게 import해줘야함
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -14,13 +15,16 @@ import routes from "./routes";
 const app = express();
 
 
+app.use(helmet());
 app.set("view engine","pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
 app.use(morgan("dev"));
 //어차피 각  router 파일 들에서 get요청에 대한 res 설정은 이미 다 되어있으니, 이미 정의된 그 로직을 미들웨어로서(?) 사용하기 위해 use를 쓰는거같음
+
+
+app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter); // use의 의미 -> 누군가 /user 경로로 접속하면 이 router 전체를 사용하겠다는 의미 , 미들웨어 전역에 사용한 use는 url을 안받았음
 app.use(routes.videos, videoRouter);
---
+++ middlewares.js
@@ -0,0 +1,7 @@
+import routes from './routes';
+
+export const localsMiddleware = (req,res,next) => {
+    res.locals.siteName = "Jotubue";
+    res.locals.routes = routes; //routes.js 의 routes 객체를 가져옴
+    next();
+};
