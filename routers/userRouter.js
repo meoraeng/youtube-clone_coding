e0200db0a943566a8f7d2f69f80977a66185d8be
joraeng <60039286+joraeng@users.noreply.github.com>
2021-03-13 12:50:52 +0900

    edit profile, change password

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,9 +9,9 @@ const userRouter = express.Router();
 //사용자 정보를 알아보기 위해 DB를 조회하는 등 더 복잡해질 수 있는 로직 
 
 userRouter.get(routes.users ,users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 
 export default userRouter
