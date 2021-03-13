9f5ef9dacba9760160fd362bb2a8b26dc3f1ecab
joraeng <60039286+joraeng@users.noreply.github.com>
2021-03-13 12:00:36 +0900

    searching (이거 이전 커밋 잘못 커밋한거라서 이게 맞음)
    진짜 깃 개 더러워서 못써먹겠네 무슨 커밋 한번 잘못 누르면 커밋 메세지를 못바꿔 ㅡㅡ

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -14,3 +14,6 @@ export const upload = (req,res) => res.render("upload",{pageTitle:"Upload Video"
 export const videoDetail = (req,res) => res.render("videoDetail", {pageTitle:"Video Deatil"});
 export const editVideo = (req,res) => res.render("editVideo", {pageTitle:"Edit Video"});
 export const deleteVideo = (req,res) => res.render("deleteVideo", {pageTitle:"Delete Video"});
+
+
+//커밋 진짜 개같네 ㅋㅋㅋㅋ 한번 실수하면 바꿀 수가 없어 ㅈ같게;;
