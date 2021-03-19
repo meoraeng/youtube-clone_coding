e6153ebb6427d2edfab19d9c459b091758b41fdb
joraeng <60039286+joraeng@users.noreply.github.com>
2021-03-20 00:32:17 +0900

    헤더 작업 끝 + 푸터 시작

--- middlewares.js
+++ middlewares.js
@@ -9,7 +9,7 @@ export const localsMiddleware = (req,res,next) => {
     res.locals.siteName = "Jotubue";
     res.locals.routes = routes; //routes.js 의 routes 객체를 가져옴
     res.locals.user = {
-        isAuthenticated: true,
+        isAuthenticated: false,
         id: 1
     }
     next();
