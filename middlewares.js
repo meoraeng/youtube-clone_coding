32cb8bfbf5d33395441c56d03e62bbd7ca4d6dc4
joraeng <60039286+joraeng@users.noreply.github.com>
2021-03-28 01:19:34 +0900

    비디오플레이어 스타일링

--- middlewares.js
+++ middlewares.js
@@ -8,7 +8,7 @@ const multerAvatar = multer({dest:'uploads/avatars/'});
 export const localsMiddleware = (req,res,next) => {
     res.locals.siteName = "Jotubue";
     res.locals.routes = routes; //routes.js 의 routes 객체를 가져옴
-    res.locals.loggedUser = req.user || null;
+    res.locals.loggedUser = req.user || {};
     next();
 };
 
